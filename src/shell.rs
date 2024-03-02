ffa7ea5f81d02159c929f0a184d86ffb4070bf3f
emmalexandria <emma.jellemabutler@gmail.com>
2024-03-03 00:28:54 +0200

    Realised my history code wasn't doing anything.

--- src/shell.rs
+++ src/shell.rs
@@ -1,12 +1,10 @@
 use crossterm::cursor::{MoveToColumn, MoveUp, SetCursorStyle};
-use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use crossterm::style::{
     Attribute, Color, ContentStyle, ResetColor, SetAttribute, SetForegroundColor, SetStyle, Stylize,
 };
 use crossterm::terminal::Clear;
 use crossterm::{execute, terminal, terminal::ClearType};
 use std::io::{self, Write};
-use std::time::Duration;
 
 use crate::rcon::RCONConnection;
 
@@ -22,12 +20,6 @@ pub struct RCONShell<'a> {
     //Used for generating a fancy little prelude to the prompt
     ip: String,
     port: u16,
-
-    /// Current user input independent of what the terminal is printing
-    curr_history_entry: String,
-    /// Line history (used for ability to seek through past commands with arrows)
-    history: Vec<String>,
-    history_offset: usize,
 }
 
 impl RCONShell<'_> {
@@ -43,10 +35,6 @@ impl RCONShell<'_> {
             stdout: io::stdout(),
             ip,
             port,
-
-            curr_history_entry: String::new(),
-            history: Vec::<String>::new(),
-            history_offset: 0,
         }
     }
 
@@ -55,76 +43,36 @@ impl RCONShell<'_> {
         match execute!(self.stdout, SetCursorStyle::SteadyBar) {
             _ => {}
         }
+        println!("{}", "\nCTRL+C or Q to quit.");
+        println!("{}", "────────────────────");
         self.shell_loop().await?;
         Ok(())
     }
 
     async fn shell_loop(&mut self) -> std::io::Result<()> {
         loop {
-            if !self.poll_events().await? {
-                println!("CTRL+C");
+            let prompt_len = self.print_prompt()?;
+
+            let mut line = String::new();
+            io::stdin().read_line(&mut line)?;
+
+            let command = line.trim();
+            if command == "Q" {
                 break;
             }
 
-            let prompt_len = self.print_prompt()?;
-
-            self.stdout.flush()?;
-
-            io::stdin().read_line(&mut self.curr_history_entry)?;
-
-            let line = self.curr_history_entry.trim().to_string();
-            self.curr_history_entry.clear();
             let last_lines = self.split_input(prompt_len, line.clone())?;
 
             execute!(self.stdout, MoveUp((last_lines) as u16))?;
             execute!(self.stdout, Clear(ClearType::FromCursorDown))?;
 
-            let res = self.conn.send_command(&line).await?;
-            self.add_history_line(line, res)?;
+            let res = self.conn.send_command(&command).await?;
+            self.add_history_line(command.to_string(), res)?;
 
             self.stdout.flush()?;
         }
 
         Ok(())
-    }
-
-    //enter the shell's loop, returns false if the loop should exit
-    async fn poll_events(&mut self) -> std::io::Result<bool> {
-        if poll(Duration::from_millis(50)).is_ok() {
-            let event = read().unwrap();
-
-            match event {
-                Event::Key(KeyEvent {
-                    code,
-                    kind: KeyEventKind::Press,
-                    modifiers: KeyModifiers::NONE,
-                    ..
-                }) => return self.handle_char_events(code),
-                Event::Key(KeyEvent {
-                    code: KeyCode::Char('c'),
-                    modifiers: KeyModifiers::CONTROL,
-                    ..
-                }) => {
-                    return Ok(false);
-                }
-                _ => {}
-            }
-        }
-
-        Ok(true)
-    }
-
-    fn handle_char_events(&mut self, code: KeyCode) -> std::io::Result<bool> {
-        match code {
-            KeyCode::Up => {
-                self.seek_up_history();
-            }
-            KeyCode::Down => {
-                self.seek_down_history();
-            }
-            _ => {}
-        }
-        Ok(true)
     }
 
     //returns the number of lines that a given input would be split over if printed next to the prompt
@@ -178,30 +126,6 @@ impl RCONShell<'_> {
         return false;
     }
 
-    //Theres a suprising amount of weirdness associated with seeking up and down the history, so these functions aren't as simple as you'd expect.
-    fn seek_up_history(&mut self) {
-        if self.history_offset < self.history.len() {
-            self.history_offset += 1;
-            //Seek from the end of the history, otherwise we'd get older items first
-            self.curr_history_entry =
-                self.history[self.history.len() - self.history_offset].clone();
-        }
-    }
-
-    fn seek_down_history(&mut self) {
-        //If we can subtract by 1, do so
-        if self.history_offset > 0 {
-            self.history_offset -= 1;
-
-            if self.history_offset == 0 {
-                self.curr_history_entry = String::new();
-            }
-
-            self.curr_history_entry =
-                self.history[self.history.len() - self.history_offset].clone();
-        }
-    }
-
     ///Adds a line to the history vec and prints it to the screen.
     //There's a lot of ugliness in this function purely due to my desire to parse the unknown command response
     //and print something nicer
@@ -234,8 +158,6 @@ impl RCONShell<'_> {
         }
         self.stdout.write(&[b'\n'])?;
 
-        self.history.push(command);
-
         Ok(())
     }
 
@@ -257,8 +179,7 @@ impl RCONShell<'_> {
     }
 
     fn gen_prompt_addr(&self) -> Vec<u8> {
-        let prompt_string =
-            String::from("[".to_owned() + &self.ip + ":" + self.port.to_string().as_str() + "] ");
+        let prompt_string = String::from("[".to_owned() + &self.ip + "] ");
         return prompt_string.as_bytes().to_vec();
     }
 }
